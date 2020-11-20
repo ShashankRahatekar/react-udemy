341b6bacf00b9b345a4e47c95f4ead8dec854994
srahatekar <shashank.rahatekar@vendekin.com>
2020-11-20 18:25:13 +0530

    added Spinner Component and Some Css

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = { lat: null, errorMessage: '' };
+	}
 
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
 			position => this.setState({ lat: position.coords.latitude }),
-			err => {
-				this.setState({ errorMessage: err.message });
-			}
+			err => this.setState({ errorMessage: err.message })
 		);
 	}
 
@@ -22,10 +23,10 @@ class App extends React.Component {
 		}
 		
 		if( !this.state.errorMessage && this.state.lat ) {
-			return (<div> Latitude: {this.state.lat} </div>);
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
-		return (<div> Loading! </div>);
+		return <Spinner message = 'Please accept location request'/>;
 	}
 }
 
